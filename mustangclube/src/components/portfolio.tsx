7d705dee25965c305b25d39284cedf04f2b096bf
luis-maia-minbdprober <luis.maia@mediaprobe.com>
2024-08-16 15:03:24 +0100

    Updated images

--- mustangclube/src/components/portfolio.tsx
+++ mustangclube/src/components/portfolio.tsx
@@ -3,77 +3,106 @@ import Marquee from "@/components/magicui/marquee";
 
 const reviews = [
   {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
+    img: "/portfolio/portfolio-1.jpg",
   },
   {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
+    img: "/portfolio/portfolio-2.jpg",
   },
   {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
+    img: "/portfolio/portfolio-3.jpg",
   },
   {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
+    img: "/portfolio/portfolio-4.jpg",
   },
   {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
+    img: "/portfolio/portfolio-5.jpg",
   },
   {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
+    img: "/portfolio/portfolio-6.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-7.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-8.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-9.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-10.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-11.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-12.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-13.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-14.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-15.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-16.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-17.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-18.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-19.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-20.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-21.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-22.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-23.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-24.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-25.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-26.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-27.jpg",
+  },
+  {
+    img: "/portfolio/portfolio-28.jpg",
   },
 ];
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
 
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
+const ReviewCard = ({ img }: { img: string }) => {
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "relative w-32 h-32 cursor-pointer overflow-hidden rounded-lg border",
         // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
       )}
     >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+      <img className="object-cover w-full h-full" alt="" src={img} />
     </figure>
   );
 };
@@ -82,13 +111,13 @@ export function MarqueeDemo() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {firstRow.map((review, index) => (
+          <ReviewCard key={index} img={review.img} />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {secondRow.map((review, index) => (
+          <ReviewCard key={index} img={review.img} />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
